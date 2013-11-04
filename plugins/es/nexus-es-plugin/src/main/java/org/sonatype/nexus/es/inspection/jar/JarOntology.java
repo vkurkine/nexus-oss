e4da49954f03764a7e7c1b5c01165de7ff0960db
Tamas Cservenak <tamas@cservenak.net>
2013-11-04 12:11:40 +0100

    Return zip and jar inspectors TBD

---
+++ plugins/es/nexus-es-plugin/src/main/java/org/sonatype/nexus/es/inspection/jar/JarOntology.java
@@ -0,0 +1,13 @@
+package org.sonatype.nexus.es.inspection.jar;
+
+/**
+ * JAR names.
+ */
+public class JarOntology
+{
+  String JAR_PREFIX = "jar.";
+
+  String ENTRIES = JAR_PREFIX + "entries";
+
+  String MANIFEST = JAR_PREFIX + "manifest";
+}
---
+++ plugins/es/nexus-es-plugin/src/main/java/org/sonatype/nexus/es/inspection/zip/ZipOntology.java
@@ -0,0 +1,11 @@
+package org.sonatype.nexus.es.inspection.zip;
+
+/**
+ * ZIP names.
+ */
+public class ZipOntology
+{
+  String ZIP_PREFIX = "zip.";
+
+  String ENTRIES = ZIP_PREFIX + "entries";
+}
