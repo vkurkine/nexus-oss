31125d8148e3e9f47ab923a7669f7e54170ebfe6
Jason Dillon <jason@planet57.com>
2015-01-18 15:16:54 -0800

    Remove annotaions from ExceptionThrowingMockRealm, appears unused

--- components/nexus-security/src/test/java/org/sonatype/security/realms/ExceptionThrowingMockRealm.java
+++ components/nexus-security/src/test/java/org/sonatype/security/realms/ExceptionThrowingMockRealm.java
@@ -12,30 +12,21 @@
  */
 package org.sonatype.security.realms;
 
-import javax.enterprise.inject.Typed;
-import javax.inject.Named;
-import javax.inject.Singleton;
-
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.realm.Realm;
 import org.apache.shiro.subject.PrincipalCollection;
 
-@Singleton
-@Typed(Realm.class)
-@Named("ExceptionThrowingMockRealm")
+// FIXME: This appears unused
+
 public class ExceptionThrowingMockRealm
     extends AuthorizingRealm
 {
-
   @Override
-  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
-      throws AuthenticationException
-  {
+  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
     throw new AuthenticationException("This realm only throws exceptions");
   }
 
@@ -43,5 +34,4 @@ public class ExceptionThrowingMockRealm
   protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
     throw new AuthorizationException("This realm only throws exceptions");
   }
-
 }
